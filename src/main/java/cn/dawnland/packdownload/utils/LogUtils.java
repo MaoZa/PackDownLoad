b40d20fda0c792f686b1249f30f121abb8697f7c
Cap_Sub <462721276@qq.com>
2020-02-22 17:29:10 +0800

    修改日志为追加模式

--- src/main/java/cn/dawnland/packdownload/utils/LogUtils.java
+++ src/main/java/cn/dawnland/packdownload/utils/LogUtils.java
@@ -13,37 +13,35 @@ public class LogUtils {
 
     public static void error(Exception e){
         File file = new File(DownLoadUtils.getRootPath() + "/error.txt");
-        FileOutputStream fos = null;
+        FileWriter fileWriter = null;
         try {
-            fos = new FileOutputStream(file);
-        } catch (FileNotFoundException e1) {
+            fileWriter = new FileWriter(file, true);
+            fileWriter.append(e.getLocalizedMessage());
+        } catch (Exception e1) {
             e1.printStackTrace();
         }
-        PrintStream ps = new PrintStream(fos);
-        e.printStackTrace(ps);
     }
 
     public static void error(String msg){
         File file = new File(DownLoadUtils.getRootPath() + "/error.txt");
-        FileOutputStream fos = null;
+        FileWriter fileWriter = null;
         try {
-            fos = new FileOutputStream(file);
-        } catch (FileNotFoundException e) {
+            fileWriter = new FileWriter(file, true);
+            fileWriter.append(msg);
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        PrintStream ps = new PrintStream(fos);
-        ps.append(LocalDateTime.now() + ": " + msg);
     }
 
     public static void info(String msg) {
         File file = new File(DownLoadUtils.getRootPath() + "/info.txt");
-        PrintStream ps = null;
+        FileWriter fileWriter = null;
         try {
-            ps = new PrintStream(file);
-        } catch (FileNotFoundException e) {
+            fileWriter = new FileWriter(file, true);
+            fileWriter.append(msg + "\n");
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        ps.append(LocalDateTime.now() + ": " + msg);
         System.out.println(LocalDateTime.now() + ": " + msg);
     }
 
